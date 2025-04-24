137f61b48bb7e0242e10f9c2c4527b8e2100f61b
Sultan-mia <sultanmia2257@gmail.com>
2025-04-24 18:49:24 +0600

    update code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const itemInfo = document.querySelector('#productInfo')
 
 const device = {
     Xiaomi_Poco_C71:{
-        Brand: 'Xiaomi', 
+        Brand: 'Xiaomi',
         model:'Xiaomi Poco C71',
         Category: 'Smartphones',
         Display:'88" 720x1640 pixels',
@@ -15,53 +15,45 @@ const device = {
 
     Xiaomi_Redmi_Note_14S:{
         Brand: 'Xiaomi',
-     model:'Xiaomi Redmi Note 14S',
-     Category: 'Smartphones',
-     Display:'6.67" 1080x2400 pixels',
-     Camera :'200MP 1080p',
-     RAM:'8GB RAM Helio G99 Ultra',
-     Battery:'5000mAh 67W',
-     img: 'img/Xiaomi-Redmi-Note-14S-Ocean-Blue.webp'
- }
-    
+        model:'Xiaomi Redmi Note 14S',
+        Category: 'Smartphones',
+        Display:'6.67" 1080x2400 pixels',
+        Camera :'200MP 1080p',
+        RAM:'8GB RAM Helio G99 Ultra',
+        Battery:'5000mAh 67W',
+        img: 'img/Xiaomi-Redmi-Note-14S-Ocean-Blue.webp'
+    }
 }
 
 selectorValue.addEventListener('change',function(){
     const collectedvalue = this.value
 
-    if(collectedvalue==='Xiaomi Poco C71'){
-        const product = device.Xiaomi_Poco_C71
-        itemInfo.innerHTML = `
-       <img src="${product.img}" alt="${product.model}" class="product-img">
-    <table class="p-table">
-        <tr><th>Field</th><th>Value</th></tr>
-        <tr><td>Brand</td><td>${product.Brand}</td></tr>
-        <tr><td>Model</td><td>${product.model}</td></tr>
-        <tr><td>Category</td><td>${product.Category}</td></tr>
-        <tr><td>Display</td><td>${product.Display}</td></tr>
-        <tr><td>Battery</td><td>${product.Battery}</td></tr>
-        <tr><td>RAM</td><td>${product.RAM}</td></tr>
-        <tr><td>Camera</td><td>${product.Camera}</td></tr>
-    </table>
-`;
+    if(collectedvalue==='Xiaomi Poco C7'){
+        updateProductInfo(device.Xiaomi_Poco_C71)
+        itemInfo.style.display = 'block';
     }
 
     else if(collectedvalue==='Xiaomi Redmi Note 14S'){
-        const product = device.Xiaomi_Redmi_Note_14S
-        itemInfo.innerHTML = `
-       <img src="${product.img}" alt="${product.model}" class="product-img">
-    <table class="p-table">
-        <tr><th>Field</th><th>Value</th></tr>
-        <tr><td>Brand</td><td>${product.Brand}</td></tr>
-        <tr><td>Model</td><td>${product.model}</td></tr>
-        <tr><td>Category</td><td>${product.Category}</td></tr>
-        <tr><td>Display</td><td>${product.Display}</td></tr>
-        <tr><td>Battery</td><td>${product.Battery}</td></tr>
-        <tr><td>RAM</td><td>${product.RAM}</td></tr>
-        <tr><td>Camera</td><td>${product.Camera}</td></tr>
-    </table>
-`;
+        updateProductInfo(device. Xiaomi_Redmi_Note_14S)
+        itemInfo.style.display = 'block';
     }
+});
 
-})
+
+function updateProductInfo(product){
+    document.querySelector('#productImage').src = product.img
+    document.querySelector('#productImage').alt = product.model;
+
+
+    //! details
+
+    document.querySelector('#productBrand').textContent = product.Brand
+    document.querySelector('#productModel').textContent = product.model
+    document.querySelector('#productCategory').textContent = product.Category
+    document.querySelector('#productDisplay').textContent = product.Display
+    document.querySelector('#productBattery').textContent = product.Battery
+    document.querySelector('#productRAM').textContent = product.RAM
+    document.querySelector('#productCamera').textContent = product.Camera
+
+}
 
